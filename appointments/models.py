ef40cd33ea01da61b65eebdcdfec1d6a7009c4ea
MMiszy <lupinek7@gmail.com>
2012-11-13 17:31:42 +0100

    Add separate field for time and for date

--- appointments/models.py
+++ appointments/models.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import User
 
 class Appointment(models.Model):
 	title = models.CharField(max_length=50)
-	date = models.DateTimeField('time of the appointment')
+	date = models.DateField('date of the appointment')
+	time = models.TimeField('time of the appointment')
 	notes = models.TextField(max_length=500)
 	author = models.ForeignKey(User)
 	def __unicode__(self):
