1f7a61ecacacfdbffd0db58ec80d1d6189c1eaae
MMiszy <lupinek7@gmail.com>
2012-11-13 14:52:12 +0100

    Add possibility to restart apache from sync script

--- sync.sh
+++ sync.sh
@@ -11,4 +11,10 @@ echo "Copying changed files to server"
 rsync -rptlh --force --progress --delete --exclude="static" ${CURRENTDIR}/* ${REMOTE_DEST}/home/mmiszy/webapps/medica/medico/
 rsync -rptlh --force --progress --delete ${CURRENTDIR}/static/* ${REMOTE_DEST}/home/mmiszy/webapps/static_media/
 
+if [[ $1 == "restart" ]]; then
+	#statements
+	echo "Restarting apache"
+	ssh ${REMOTE_HOST} '/home/mmiszy/webapps/medica/apache2/bin/restart'
+fi
+
 popd
