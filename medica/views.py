239216bbd90b36f2dbe426bd47bb86d11ca0132a
MMiszy <lupinek7@gmail.com>
2012-12-19 21:02:11 +0100

    Add proper RequestContext to the index view

--- medica/views.py
+++ medica/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 from appointments.models import *
 import datetime
 
@@ -9,4 +10,4 @@ def current_datetime(request):
     return HttpResponse(html)
 
 def index(req):
-	return render_to_response('index.html')
+	return render_to_response('index.html',context_instance=RequestContext(req))
